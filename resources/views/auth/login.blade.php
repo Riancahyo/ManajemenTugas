12ed74d603eba3945ec274da461f268abbe9dea0
Riancahyo <riancahyo75@gmail.com>
2024-10-20 11:46:50 +0700

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
+<div class="container mt-4">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6"> <!-- Mempertahankan lebar container -->
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
@@ -12,9 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-left ml-2" style="padding-right: 0;">{{ __('Email') }}</label> 
+                            <!-- Mengurangi padding kanan untuk label agar lebih dekat ke input -->
 
-                            <div class="col-md-6">
+                            <div class="col-md-7" style="padding-left: 0;"> <!-- Mengurangi padding kiri pada input -->
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -26,9 +27,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-left ml-2" style="padding-right: 0;">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-7" style="padding-left: 0;">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -40,7 +41,7 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-8 mt-2 mb-2 ml-2">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
